b945b734b56cd780a36c6e63ce8008b95ec0f6ff
Harshal Chavan <harshalchavan132@outlook.com>
2025-06-20 00:05:45 +0400

    playwright execution basics

---
+++ javascript/basics.js
@@ -0,0 +1,17 @@
+
+console.log('learning playwright')
+
+// Playwright documentation
+/**
+ * https://playwright.dev/docs/intro
+ * https://playwright.dev/docs/api/class-playwright
+ * 
+ */
+
+// Playwright execution process
+
+/**
+ * npx playwright test              -> To execute test cases from package added in config file
+ * npx playwright test --headed     -> To execute test cases in headed mode
+ * npx playwright test example      -> To execute tests from specific file
+ */
